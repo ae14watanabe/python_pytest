bdd17e7b08cc32fc565cdd9b630abbb5d10d677d
Ryuji Watanabe <ryujisan@pepabo.com>
2021-08-17 14:38:37 +0900

    サンプルのテストが実行されるとわかりにくいので全てコメントアウトする

--- tests/acme/test_snake.py
+++ tests/acme/test_snake.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
 
-"""
-サンプルテスト
-"""
+# """
+# サンプルテスト
+# """
 
-import pytest
-from acme.snake import Python, MontyPython
-
-
-def test_python_hisses():
-    sut = Python()
-    actual = sut.say()
-    assert 'Hello' not in actual, 'PythonはHelloと言わない'
+# import pytest
+# from acme.snake import Python, MontyPython
 
 
-@pytest.fixture
-def monty_python():
-    return MontyPython()
+# def test_python_hisses():
+#     sut = Python()
+#     actual = sut.say()
+#     assert 'Hello' not in actual, 'PythonはHelloと言わない'
 
 
-class TestMontyPython:
-    def test_say_name_guido(self, monty_python):
-        actual = monty_python.say('Guido')
-        assert actual == 'Hello Guido'
+# @pytest.fixture
+# def monty_python():
+#     return MontyPython()
 
-    def test_say_name_kent(self, monty_python):
-        actual = monty_python.say('Kent')
-        assert actual == 'Hello Kent'
 
-    @pytest.mark.parametrize(
-        "name, expected",
-        [
-            ('Terry', 'Hello Terry'),
-            ('John', 'Hello John'),
-        ]
-    )
-    def test_say_name(self, monty_python, name, expected):
-        actual = monty_python.say(name)
-        assert actual == expected
+# class TestMontyPython:
+#     def test_say_name_guido(self, monty_python):
+#         actual = monty_python.say('Guido')
+#         assert actual == 'Hello Guido'
+
+#     def test_say_name_kent(self, monty_python):
+#         actual = monty_python.say('Kent')
+#         assert actual == 'Hello Kent'
+
+#     @pytest.mark.parametrize(
+#         "name, expected",
+#         [
+#             ('Terry', 'Hello Terry'),
+#             ('John', 'Hello John'),
+#         ]
+#     )
+#     def test_say_name(self, monty_python, name, expected):
+#         actual = monty_python.say(name)
+#         assert actual == expected
